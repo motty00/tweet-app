27e86c47bdfd78a33de501f3e74ddc9b6febd475
motty00 <kowappa1104@yahoo.co.jp>
2021-02-24 12:00:23 +0900

    tweetsテーブルに外部キー（user_id）を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_014139) do
+ActiveRecord::Schema.define(version: 2021_02_24_025559) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
