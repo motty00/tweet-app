3105a86c720d8e7caa60d927e7ec5c919bd260fb
motty00 <kowappa1104@yahoo.co.jp>
2021-02-26 17:30:32 +0900

    お気に入りのカウントを非同期で実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,14 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!
   before_action :like_params, only: [:show]
+  before_action :tweet_params
 
   def create
     Like.create(user_id: current_user.id, tweet_id: params[:id])
-    redirect_to root_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, tweet_id: params[:id]).destroy
-    redirect_to root_path
   end
 
   def index
@@ -29,6 +28,10 @@ class LikesController < ApplicationController
     if user_signed_in? 
       @likes = Like.where(user_id: current_user.id).count
     end
+  end
+
+  def tweet_params
+    @tweet = Tweet.find(params[:id])
   end
 
 end
