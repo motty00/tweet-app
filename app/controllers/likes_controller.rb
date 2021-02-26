4c11a12eeef86cbdc244893641cbb52e8f65955b
motty00 <kowappa1104@yahoo.co.jp>
2021-02-26 11:41:09 +0900

    commit

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,33 @@
+class LikesController < ApplicationController
+  before_action :like_params, only: [:show]
+
+  def create
+    Like.create(user_id: current_user.id, tweet_id: params[:id])
+    redirect_to root_path
+  end
+
+  def destroy
+    Like.find_by(user_id: current_user.id, tweet_id: params[:id]).destroy
+    redirect_to root_path
+  end
+
+  def index
+    @likes = Like.find_by(user_id: current_user.id)
+  end
+
+  def show
+    likes = Like.where(user_id: current_user.id)
+    like = likes.select(:tweet_id)
+    @tweets = Tweet.where(id: like).order('created_at desc')
+  end
+
+
+
+
+  def like_params
+    if user_signed_in? 
+      @likes = Like.where(user_id: current_user.id).count
+    end
+  end
+
+end
