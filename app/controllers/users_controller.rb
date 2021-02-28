f93a4e1afd9b891e7b64b3bc1418485f85033abc
motty00 <kowappa1104@yahoo.co.jp>
2021-02-28 20:36:09 +0900

    userコントローラー、viewsファイルのコメントアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @nickname = @user.nickname
     @introduction = @user.self_introduction
-    @tweets = Tweet.all
-    if user_signed_in? 
+    @tweets = Tweet.all #全ての投稿を取得
+    if user_signed_in? #ログイン状態のユーザーであればお気に入りに登録したレコードを取得
       @likes = Like.where(user_id: current_user.id)
     end
   end
