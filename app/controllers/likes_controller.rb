af31fbcbae7f31879dc5741da3e72a421bfb3a23
motty00 <kowappa1104@yahoo.co.jp>
2021-02-28 21:21:20 +0900

    likesテーブル、viewsファイルのコメントアウト

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,21 +4,21 @@ class LikesController < ApplicationController
   before_action :tweet_params, except: [:show]
 
   def create
-    Like.create(user_id: current_user.id, tweet_id: params[:id])
+    Like.create(user_id: current_user.id, tweet_id: params[:id]) #ユーザーIDがログイン中のユーザー、投稿IDが選択した投稿IDのお気に入りを追加
   end
 
   def destroy
-    Like.find_by(user_id: current_user.id, tweet_id: params[:id]).destroy
+    Like.find_by(user_id: current_user.id, tweet_id: params[:id]).destroy #ユーザーIDがログイン中のユーザー、投稿IDが選択した投稿IDのお気に入りを削除
   end
 
   def index
-    @likes = Like.find_by(user_id: current_user.id)
+    @likes = Like.find_by(user_id: current_user.id) #likesテーブルからuser_idがログインユーザーIDのデータを取得
   end
 
   def show
-    likes = Like.where(user_id: current_user.id)
-    like = likes.select(:tweet_id)
-    @tweets = Tweet.where(id: like)
+    likes = Like.where(user_id: current_user.id) #likesテーブルからuser_idがログインユーザーのレコードを取得
+    like = likes.select(:tweet_id) #取得したレコードのtweet_idを取得（変数likeにはtweet_idの数値が入る）
+    @tweets = Tweet.where(id: like) #tweetsテーブルからidが変数likeに入っているレコードを取得
   end
 
 
@@ -26,7 +26,7 @@ class LikesController < ApplicationController
 
   def like_params
     if user_signed_in? 
-      @likes = Like.where(user_id: current_user.id)
+      @likes = Like.where(user_id: current_user.id) #ログイン状態ならlikesテーブルから自分のユーザーIDのレコードを取得
     end
   end
 
