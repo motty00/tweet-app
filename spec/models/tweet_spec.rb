92663810be06cc607bb999d96ce12ba91ab504b4
motty00 <kowappa1104@yahoo.co.jp>
2021-03-01 18:04:29 +0900

    tweetモデルの単体テストコード

---
+++ spec/models/tweet_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+RSpec.describe Tweet, type: :model do
+  before do
+    @tweet = FactoryBot.build(:tweet)
+  end
+
+  describe '新規登録' do
+    context '新規投稿できるとき' do
+      it 'textが存在すれば登録できる' do
+        expect(@tweet).to be_valid
+      end
+    end
+    context '新規投稿できないとき' do
+      it 'textが空だと登録できない' do
+        @tweet.text = ''
+        @tweet.valid?
+        expect(@tweet.errors.full_messages).to include("Text can't be blank")
+      end
+    end
+  end
+end
