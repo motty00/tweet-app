455ea886f8a2e123fa645f4944d6edae0341c199
motty00 <kowappa1104@yahoo.co.jp>
2021-03-03 20:57:18 +0900

    マイページからのツイート詳細リンクを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @nickname = @user.nickname
     @introduction = @user.self_introduction
-    @tweets = Tweet.all # 全ての投稿を取得
+    @tweets = Tweet.all.order('created_at desc') # 全ての投稿を取得
     @likes = Like.where(user_id: current_user.id) if user_signed_in? # ログイン状態のユーザーであればお気に入りに登録したレコードを取得
   end
 end
