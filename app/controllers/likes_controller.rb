f777fba1fe3be5d817edf4b719eb20e92d7b74e9
motty00 <kowappa1104@yahoo.co.jp>
2021-03-05 21:30:27 +0900

    likesコントローラー内の共通するアクションをprivate内に記載

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,24 +2,21 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_like, only: [:show]
   before_action :set_tweet, except: [:show]
+  before_action :like_where, only: [:create, :destroy, :index, :show]
 
   def create
     Like.create(user_id: current_user.id, tweet_id: params[:id]) # ユーザーIDがログイン中のユーザー、投稿IDが選択した投稿IDのお気に入りを追加
-    @likes = Like.where(user_id: current_user.id)
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, tweet_id: params[:id]).destroy # ユーザーIDがログイン中のユーザー、投稿IDが選択した投稿IDのお気に入りを削除
-    @likes = Like.where(user_id: current_user.id)
   end
 
   def index
-    @likes = Like.where(user_id: current_user.id) # likesテーブルからuser_idがログインユーザーIDのデータを取得
   end
 
   def show
-    likes = Like.where(user_id: current_user.id) # likesテーブルからuser_idがログインユーザーのレコードを取得
-    like = likes.select(:tweet_id) # 取得したレコードのtweet_idを取得（変数likeにはtweet_idの数値が入る）
+    like = @likes.select(:tweet_id) # 取得したレコードのtweet_idを取得（変数likeにはtweet_idの数値が入る）
     @tweets = Tweet.where(id: like) # tweetsテーブルからidが変数likeに入っているレコードを取得
   end
 
@@ -30,6 +27,10 @@ class LikesController < ApplicationController
     if user_signed_in?
       @likes = Like.where(user_id: current_user.id) # ログイン状態ならlikesテーブルから自分のユーザーIDのレコードを取得
     end
+  end
+
+  def like_where
+    @likes = Like.where(user_id: current_user.id) # likesテーブルからuser_idがログインユーザーIDのデータを取得
   end
 
   def set_tweet
