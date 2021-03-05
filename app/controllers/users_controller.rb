b463bff59d87833d2cc7f1ec14ad342ab801a9a4
motty00 <kowappa1104@yahoo.co.jp>
2021-03-05 21:15:09 +0900

    usersコントローラーの記述を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def show
-    @user = User.find(params[:id])
-    @nickname = @user.nickname
-    @introduction = @user.self_introduction
+    @user = User.find(params[:id]) #特定のユーザー情報を取得
     @tweets = Tweet.all.order('created_at desc') # 全ての投稿を取得
     @likes = Like.where(user_id: current_user.id) if user_signed_in? # ログイン状態のユーザーであればお気に入りに登録したレコードを取得
   end
