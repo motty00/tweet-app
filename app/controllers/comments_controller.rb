2a4dd45892efc9adf9fc469605fd4701104ee19f
motty00 <kowappa1104@yahoo.co.jp>
2021-03-05 19:03:12 +0900

    コメントを行ったユーザーであればコメントの削除ができる機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,12 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to root_path
+  end
+
 
 
   private
