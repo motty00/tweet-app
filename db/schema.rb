f234a44a35afe8870c9e2a0f54a384449bc12d8c
motty00 <kowappa1104@yahoo.co.jp>
2021-03-09 19:11:29 +0900

    dbの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_015421) do
+ActiveRecord::Schema.define(version: 2021_03_09_100846) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
