f8d6812a952acdf36ade61c6e470d42970caa60c
motty00 <kowappa1104@yahoo.co.jp>
2021-03-22 16:47:40 +0900

    中間テーブル、アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,11 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments
 
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
   def liked_by?(tweet_id)
     likes.where(tweet_id: tweet_id).exists? # likesテーブルにtweet_idが存在するか検索をかけている
   end
